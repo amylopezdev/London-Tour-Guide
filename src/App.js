ee4655ec923129c0f58a82cb236db8c5e5a94b70
Amy Lopez <amy.lopez@sainsburys.co.uk>
2022-07-18 19:28:39 +0100

    resolve warning in useEffect

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ export const App = () => {
   useEffect(() => {
     setCategories(allCategories);
     setSelectedCategory("all")
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const filterItems = (category) => {
